09d8bf86d0261a1e7fecdd4c4107c2879a04f531
ToberJakub <jakubtober276@gmail.com>
2024-05-06 19:51:12 +0200

    Adding download function with

--- main.py
+++ main.py
@@ -1,8 +1,25 @@
 from tkinter import *
 from tkinter import filedialog
+from pytube import YouTube
+from moviepy.editor import *
+import shutil
+
+
+def download():
+    video_path = url_entry.get()
+    file_path = path_label.cget("text")
+    print('Downloading...')
+    YouTube(video_path).streams.get_highest_resolution().download()
+    video_clip = VideoFileClip(mp4)
+    video_clip.close()
+    shutil.move(mp4, file_path)
+    print('Download Complete!')
+
+def get_path():
+    path = filedialog.askdirectory()
+    path_label.config(text=path)
 
 root = Tk()
-
 root.title('Video Downloader')
 canvas = Canvas(root, width=400, height=300)
 canvas.pack()
@@ -19,11 +36,11 @@ canvas.create_window(200, 100, window=url_entry)
 
 #path to download videos
 path_label = Label(root, text='Selcet path to download')
-path_button = Button(root,text='Select')
+path_button = Button(root,text='Select',command=get_path)
 canvas.create_window(200, 150, window=path_label)
 canvas.create_window(200, 170, window=path_button)
 
 #download button
-download_button = Button(root,text='Download')
-canvas.create_window(200, 250, window=path_button)
+download_button = Button(root,text='Download', command='download')
+canvas.create_window(200, 250, window=download_button)
 mainloop()
